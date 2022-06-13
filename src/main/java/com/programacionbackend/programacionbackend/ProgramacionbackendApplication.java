b51fdbe11008bf615ce8e15b40d89da200fa27ac
Franco Morales Castro <franco.morales@outlook.com>
2022-06-13 18:24:15 -0400

    feat: initial project

---
+++ src/main/java/com/programacionbackend/programacionbackend/ProgramacionbackendApplication.java
@@ -0,0 +1,13 @@
+package com.programacionbackend.programacionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacionbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramacionbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programacionbackend/programacionbackend/ProgramacionbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacionbackend.programacionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacionbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
