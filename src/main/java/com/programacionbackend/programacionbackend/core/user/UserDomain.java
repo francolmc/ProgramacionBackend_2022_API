7f125d6c5fc452f7ce506954a9bc89fa2a16e65c
francolmc <franco.morales@outlook.com>
2022-07-25 03:06:05 -0400

    feat: jwt

--- src/main/java/com/programacionbackend/programacionbackend/core/user/UserDomain.java
+++ src/main/java/com/programacionbackend/programacionbackend/core/user/UserDomain.java
@@ -1,7 +1,6 @@
 package com.programacionbackend.programacionbackend.core.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
