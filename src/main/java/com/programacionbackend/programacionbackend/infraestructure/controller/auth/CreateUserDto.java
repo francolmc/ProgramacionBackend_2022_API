160f74b7df7a6fdead74339f4db45b34ed2bf7d2
francolmc <franco.morales@outlook.com>
2022-07-25 02:52:32 -0400

    feat: jwt

--- src/main/java/com/programacionbackend/programacionbackend/infraestructure/controller/auth/CreateUserDto.java
+++
@@ -1,8 +0,0 @@
-package com.programacionbackend.programacionbackend.infraestructure.controller.auth;
-
-public class CreateUserDto {
-    public String email;
-    public String firstName;
-    public String lastName;
-    public String password;
-}
--- src/main/java/com/programacionbackend/programacionbackend/service/JwtUserDetailsService.java
+++ src/main/java/com/programacionbackend/programacionbackend/service/JwtUserDetailsService.java
@@ -1,10 +1,8 @@
 package com.programacionbackend.programacionbackend.service;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,19 +13,18 @@ import com.programacionbackend.programacionbackend.infraestructure.database.mode
 import com.programacionbackend.programacionbackend.infraestructure.database.repository.UserEntityJpaRepository;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+public class JwtUserDetailsService implements UserDetailsService {
     @Autowired
     private UserEntityJpaRepository userEntityJpaRepository;
 
-    @Override
+	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        List<SimpleGrantedAuthority> roles = null;
-		UserEntity user = this.userEntityJpaRepository.findUserByEmail(username);
-
-        if(user != null) {
-            roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-            return new User(user.getEmail(), user.getPassword(), roles);
-        }
-		throw new UsernameNotFoundException("User not found with email: " + username);
+        UserEntity user = this.userEntityJpaRepository.findUserByEmail(username);
+		if (user != null) {
+			return new User(user.getEmail(), user.getPassword(),
+					new ArrayList<>());
+		} else {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
 	}
 }
